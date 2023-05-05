bf75d98b8104924ce4f54d9f3ab6d2c8b4be3a45
Trần Thế Anh <trtheanh.work@gmail.com>
2023-05-05 10:51:15 +0700

    update in hotelBooking - booking time input

--- src/components/Input/CalendarInput.js
+++ src/components/Input/CalendarInput.js
@@ -7,6 +7,10 @@ import "react-datepicker/dist/react-datepicker.css";
 export default function CalendarInput(props) {
     const [focus, setFocus] = React.useState(false);
 
+    let copyDate = props.selectedDate;
+    const options = { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' };
+    const formattedDate = copyDate.toLocaleDateString('vi-VN', options);
+
     const styleDivCalendarInput = {
         width: "100%",
         height: props.height,
@@ -14,9 +18,9 @@ export default function CalendarInput(props) {
         cursor: "pointer"
     }
 
-    function onChangeInput(date) {
-        // props.onChange(date);
-        console.log(date);
+    function onChangeInput(date, event) {
+        props.onChange(date.toJSON());
+        setFocus(false);
     }
 
     function focusToInput() {
@@ -26,26 +30,25 @@ export default function CalendarInput(props) {
     return (
         <div className="calendar-input" style={{width: props.width}}>
             <label htmlFor={props.name} style={{fontSize: props.fontSize, fontWeight: 600, marginBottom: "8px", marginTop: "15px"}}>{props.label}</label>
-            <div className="calendar-input--container" style={styleDivCalendarInput} onClick={focusToInput} onFocus={() => {setFocus(true)}} onBlur={() => {setFocus(false)}}>
+            <div className="calendar-input--container" style={styleDivCalendarInput} onFocus={() => {setFocus(true)}} onBlur={() => {setFocus(false)}}>
                 {props.haveIcon && 
                     <div style={{fontSize: props.fontSize}} className="calendar-input--icon" onClick={focusToInput}>
                         {props.icon}
                     </div>
                 }
 
-                {/* <input style={{fontSize: props.fontSize}} placeholder="Hà Nội" onChange={onChangeInput} type="text" id={props.name} name={props.name}/> */}
-                
                 <DatePicker 
-                    selected={props.selectedDate}
+                    shouldCloseOnSelect={true}
+                    dateFormat="cccc, MMMM dd, yyyy"
                     minDate={new Date()}
-                    onChange={(date) => onChangeInput(date)}
-                    dateFormat='dd, MM, yyyy'
+                    onChange={(date, event) => onChangeInput(date, event)}
                     className="calendar-input--container-input"
                     name={props.name}
                     id={props.name}
                     wrapperClassName="calendar-input--container-input--wrapper"
                     />
 
+                <div className="calendar-input--data-str" onClick={focusToInput} style={{fontSize: "15px"}}><span>{formattedDate}</span></div>
             </div>
         </div>
     );  
--- src/components/Main_components/Booking/HotelBooking.js
+++ src/components/Main_components/Booking/HotelBooking.js
@@ -6,29 +6,52 @@ import CalendarInput from "../../Input/CalendarInput";
 import OnlyNumberInput from "../../Input/OnlyNumberInput";
 
 export default function HotelBooking() {
-    let date = new Date();
-    const [currentDay, setCurrentDay] = React.useState(date.toJSON());
-
+    //location
     const [searchLocation, setSearchLocation] = React.useState({
         "province_id": null,
         "district_id": null,
         "sub_district_id": null
     });
 
+    function changeSearchLocation(searchLocationString) {
+        console.log(searchLocationString);
+    }
+
+    //booking time
+    let date = new Date();
+    let dateTemp = new Date(date);
+    let checkOutDateDefault = new Date(dateTemp.setDate(date.getDate() + 1));
+
+    
+
+    const [currentDay, setCurrentDay] = React.useState(date.toJSON());
+
     const [bookingTime, setBookingTime] = React.useState({
         "check_in_date": currentDay,
         "nights_number": 1,
-        "check_out_date": null,
+        "check_out_date": checkOutDateDefault.toJSON(),
     });
+
+    let copyDate = new Date(bookingTime.check_out_date);
+    const options = { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' };
+    const formattedDate = copyDate.toLocaleDateString('vi-VN', options);
+
+    function changeCheckInDate(newDate) {
+        let newCheckInDate = new Date(newDate);
+        let newDateTemp = new Date(newCheckInDate);
+        let newCheckOutDate = new Date(newDateTemp.setDate(newCheckInDate.getDate() + bookingTime.nights_number));
+        
+        setBookingTime({
+            ...bookingTime,
+            "check_in_date": newDate,
+            "check_out_date": newCheckOutDate.toJSON(),
+        });
+    }
 
     const [bookingDetail, setBookingDetail] = React.useState({
         "guest_number": 2,
         "rooms_number": 1,
     });
-
-    function chagneSearchLocation(searchLocationString) {
-        console.log(searchLocationString);
-    }
 
     //test only number input
     const [onlyNumberValue, setOnlyNumberValue] = React.useState(null);
@@ -39,12 +62,19 @@ export default function HotelBooking() {
     return (
         <div className="hotel-booking--container">
             <div className="hotel-booking--location-container">
-                <SuggestionInput width="90%" height="40px" fontSize="16px" onChange={chagneSearchLocation} name="location-input" label="Thành phố, địa điểm: " haveIcon={true} icon={<FaSearchLocation size="16px"/>} />
+                <SuggestionInput width="90%" height="40px" fontSize="16px" onChange={changeSearchLocation} name="location-input" label="Thành phố, địa điểm: " haveIcon={true} icon={<FaSearchLocation size="16px"/>} />
             </div>
 
             <div className="hotel-booking--time-container" style={{width: "90%", display: "flex", justifyContent: "space-between"}}>
-                <CalendarInput selectedDate={new Date(currentDay)} width="40%" height="40px" fontSize="16px" onChange={chagneSearchLocation} name="start-date-input" label="Ngày nhận phòng: " haveIcon={true} icon={<FaRegCalendarAlt size="16px"/>} />
-                <CalendarInput width="40%" height="40px" fontSize="16px" onChange={chagneSearchLocation} name="location-input" label="Thành phố, địa điểm: " haveIcon={true} icon={<FaRegCalendarAlt size="16px"/>} />
+                <CalendarInput selectedDate={new Date(bookingTime.check_in_date)} width="40%" height="40px" fontSize="16px" onChange={changeCheckInDate} name="start-date-input" label="Ngày nhận phòng: " haveIcon={true} icon={<FaRegCalendarAlt size="16px"/>} />
+                
+                <div className="check-out-date--container">
+                    <label style={{fontSize: "16px", fontWeight: 600, marginBottom: "16px"}}>Ngày trả phòng:</label>
+
+                    <div style={{fontSize: "15px"}}>
+                        {formattedDate}
+                    </div>
+                </div>
             </div>
             
             <div className="hotel-booking--detail-container">
