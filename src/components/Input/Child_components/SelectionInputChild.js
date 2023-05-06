8bb8845b3a0b0c38b3dfa944ad1ab4d60e952fa9
Trần Thế Anh <trtheanh.work@gmail.com>
2023-05-06 21:17:15 +0700

    update selection input

---
+++ src/components/Input/Child_components/SelectionInputChild.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "../../css/Input/Child_components/SelectionInputChild.css";
+import {GoPrimitiveDot} from "react-icons/go"
+
+export default function SelectionInputChild(props) {
+    const [hover, setHover] = React.useState(false);
+
+    const styleDivSelectionInputChild = {
+        backgroundColor: props.isSelected ? props.backgroundColorHover : (hover ? props.backgroundColorHover : props.backgroundColor),
+        cursor: "pointer",
+        color: props.isSelected ? "#2E98DD" : "black"
+    }
+
+  const handleMouseOver = () => {
+      setHover(true);
+    };
+  
+    const handleMouseOut = () => {
+      setHover(false);
+    };
+
+    function onClick(event) {
+        console.log(event);
+    }
+
+    return (
+        <div className="selection-child--container" style={styleDivSelectionInputChild} onClick={onClick} onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+            {(props.isSelected || hover) &&
+              <div className="selection-child--icon-selected">
+                <GoPrimitiveDot size={props.fontSizeTitle} />
+              </div>
+            }
+
+            <div className="selection-child--data" style={{marginLeft: props.isSelected ? "0" : (hover ? "0" : "10%")}}>
+              <div style={{fontSize: props.fontSizeTitle}} className="selection-child--title">{props.title}</div>
+              
+              {
+                props.subTitle !== "" &&
+                <div style={{fontSize: props.fontSizeSubTitle}} className="selection-child--subtitle">{props.subTitle}</div>
+              }
+
+            </div>
+        </div>
+    );  
+}
--- src/components/Input/SelectionInput.js
+++ src/components/Input/SelectionInput.js
@@ -3,10 +3,34 @@ import "../../css/Input/SelectionInput.css";
 import { CSSTransition } from "react-transition-group";
 import "../../css/Trantision.css"
 import {FaAngleDown} from "react-icons/fa"
+import SelectionInputChild from "./Child_components/SelectionInputChild";
 
 import "react-datepicker/dist/react-datepicker.css";
 
 export default function SelectionInput(props) {
+    const [componentOptionsShow, setComponentOptionsShow] = React.useState(false);
+    const ref = React.useRef(null);
+
+    function handleClickOutside(event) {
+        if (ref.current && !ref.current.contains(event.target)) {
+            setComponentOptionsShow(false);
+            setFocus(false);
+          }
+        }
+
+    React.useEffect(() => {
+        document.addEventListener("mousedown", handleClickOutside);
+        
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+        });
+
+    function showComponentOptions() {
+        setComponentOptionsShow(componentOptionsShow ? false : true);
+        setFocus(true);
+    }
+    
     const [focus, setFocus] = React.useState(false);
     let currentSelected = props.arrayOptions.find(item => item.value === props.selectedValue);
     
@@ -17,43 +41,40 @@ export default function SelectionInput(props) {
         cursor: "pointer"
     }
     
-    function onChangeInput(event) {
-        console.log(event);
-        setFocus(false);
-    }
-    
-    function focusToInput() {
-        document.getElementById(props.name).focus();
-    }
-    
     //options: title, value is required; sub_title is not required
     return (
-        <div className="selection-input" style={{width: props.width}}>
-            <label htmlFor={props.name} style={{fontSize: props.fontSize, fontWeight: 600, marginBottom: "8px", marginTop: "15px"}}>{props.label}</label>
-            <div className="selection-input--container" style={styleDivSelectionInput} onFocus={() => {setFocus(true)}} onBlur={() => {setFocus(false)}}>
+        <div ref={ref} className="selection-input" style={{width: props.width}}>
+            <label onClick={showComponentOptions} style={{fontSize: props.fontSize, fontWeight: 600, marginBottom: "8px", marginTop: "15px"}}>{props.label}</label>
+            <div onClick={showComponentOptions} className="selection-input--container" style={styleDivSelectionInput} onFocus={() => {setFocus(true)}} onBlur={() => {setFocus(false)}}>
                 {props.haveIcon && 
-                    <div style={{fontSize: props.fontSize}} className="selection-input--icon" onClick={focusToInput}>
+                    <div style={{fontSize: props.fontSize}} className="selection-input--icon">
                         {props.icon}
                     </div>
                 }
 
-                <div style={{fontSize: props.fontSize}} className="selection-input--selected-value" onClick={focusToInput}>
+                <div style={{fontSize: props.fontSize}} className="selection-input--selected-value">
                     <span>{currentSelected ? currentSelected.title : ""}</span>
                 </div>
 
-                <div style={{fontSize: props.fontSize}} className="selection-input--icon-arrow" onClick={focusToInput}>
+                <div style={{fontSize: props.fontSize}} className="selection-input--icon-arrow">
                     {<FaAngleDown size={16}/>}
                 </div>
             </div>
 
-                {/* <CSSTransition
-                    in={componentChildShow}
+                <CSSTransition
+                    in={componentOptionsShow}
                     timeout={300}
                     classNames="element--opacity-1--transition"
                     unmountOnExit
                 >
-                    {props.componentChild}
-              </CSSTransition> */}
+                    {
+                        <div className="selection-input--options-container">
+                            {props.arrayOptions.map((option) => 
+                                <SelectionInputChild isSelected={option.value === props.selectedValue} key={option.value} value={option.value} backgroundColorHover="#F2F3F3" backgroundColor="white" fontSizeTitle="16px" title={option.title} subTitle={option.sub_title ? option.sub_title : ""} fontSizeSubTitle="14px"/>
+                            )}
+                        </div>
+                    }
+              </CSSTransition>
         </div>
     );  
 }
