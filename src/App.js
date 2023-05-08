66cf9fbf74d9f1e36b43b4e855d42fa7d7ab8104
Trần Thế Anh <trtheanh.work@gmail.com>
2023-05-08 22:43:42 +0700

    add popular destinations

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import RootLayout from './layout/RootLayout';
-import MainPage from './pages/MainPage';
+import MainPage, { popularDestinationsLoader } from './pages/MainPage';
 import {  createBrowserRouter, Routes, Route, NavLink, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 import NotFound from './pages/NotFound';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
         <Route path='/' element={<RootLayout />}>
-          <Route index element={<MainPage />}/>
+          <Route 
+            index 
+            element={<MainPage />} 
+          />
           <Route path='about' element={<div style={{marginTop: "200px"}}>about</div>}/>
 
           <Route path='*' element={<NotFound />}/>
--- src/layout/RootLayout.js
+++ src/layout/RootLayout.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header_components/Header'
 import { Outlet } from 'react-router-dom'
+import Footer from '../components/Footer'
 
 export default function RootLayout() {
   return (
     <div className='root-layout'>
         <Header />
         <Outlet />
+        <Footer />
     </div>
   )
 }
